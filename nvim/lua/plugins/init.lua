64ce4e8cc12eb378c7379aeac1612d71a3031ed1
Pulkit Garg <pulkitg@sidefx.com>
2026-09-15 09:49:03 -0400

    updated plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,16 +13,35 @@ return {
     end,
   },
 
+  {
+    "mhinz/vim-signify",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = { "c", "cpp" },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufReadPost",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      enable = true,            -- Enable this plugin by default
+      max_lines = 3,            -- How many lines the floating window should span
+      min_window_height = 0,    -- Minimum editor window height to enable context
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = "outer",     -- Which context lines to discard if max_lines is exceeded
+      mode = "cursor",          -- Line used to calculate context. Choices: 'cursor', 'topline'
+    },
+  },
+
 }
